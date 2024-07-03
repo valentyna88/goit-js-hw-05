8d9d6e1e626016390600124a9b95cd8a0374d492
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-07-03 19:18:40 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,9 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-};
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .reduce((total, user) => {
+      return total + user.balance
+    }, 0);
 
 const clients = [
 	{
